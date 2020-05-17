c78bb13a6b991ce31153bc5f622aecd7c37c17ce
homkim <homkim@naver.com>
2020-05-17 18:03:59 +0900

    gugudan stringtokenizer

---
+++ com/lec/java/lab01/GugudanInputLine.java
@@ -0,0 +1,20 @@
+package com.lec.java.lab01;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class GugudanInputLine {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Input two int values: ");
+		String str = sc.nextLine();
+		
+		StringTokenizer st = new StringTokenizer(str);
+		int i = Integer.parseInt(st.nextToken());
+		int j = Integer.parseInt(st.nextToken());
+		
+		System.out.printf("%d x %d is %2d",i, j, i*j);
+		
+	}
+}
