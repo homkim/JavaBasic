c9c2f5855390408396e050216f76ecbb3307e955
homkim <homkim@naver.com>
2020-05-17 18:32:56 +0900

    gugudan exception

--- com/javalec/lab02/Gugudan.java
+++ com/javalec/lab02/Gugudan.java
@@ -3,9 +3,12 @@ package com.javalec.lab02;
 public class Gugudan {
 	public static void main(String[] args) {
 		
-		for (int i = 1; i < 10; i++) {
+		for (int i = 0; i < 10; i++) {
 			for (int j = 2; j < 10; j++) {
-				System.out.printf("%d x %d = %2d\t",j,i,j*i);
+				if (i == 0)
+					System.out.printf("%d 단\t\t",j);
+				else
+					System.out.printf("%d x %d = %2d\t",j,i,j*i);
 			}
 			System.out.println("");
 			
--- com/javalec/lab02/GugudanInput.java
+++ com/javalec/lab02/GugudanInput.java
@@ -1,5 +1,6 @@
 package com.javalec.lab02;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GugudanInput {
@@ -7,9 +8,17 @@ public class GugudanInput {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Input two values for gugudan: ");
-		int i = sc.nextInt();
-		int j = sc.nextInt();
-		System.out.printf("%d x %d is %2d",i,j,i*j);
+		int i = 0, j = 0;
+		try {
+			i = sc.nextInt();
+			j = sc.nextInt();
+			
+			System.out.printf("%d x %d is %2d", i, j, i*j );
+		} catch (InputMismatchException e) {
+			// TODO: handle exception
+			System.out.println("구구단은 두개의 숫자를 입력합니다. 숫자를 입력하세요");
+		}
+		
 	}
 
 }
